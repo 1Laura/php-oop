f50787cc609f1350f87e6a4ce6b120f7eaea2914
Laura_Bal <laurab@theimpactbrands.com>
2023-10-18 22:28:40 +0300

    Add a constructor

--- objects.php
+++ objects.php
@@ -35,3 +35,5 @@ $vehicle3 = new Vehicle("Sage", "Ecostar", "2014", "70000", "15000", "veh-03.jpg
 //$vehicle2->mileage = "30000";
 //$vehicle2->price = "31000";
 //$vehicle2->image = "veh-02.jpg";
+
+$vehicle=[$vehicle1, $vehicle2, $vehicle3];
